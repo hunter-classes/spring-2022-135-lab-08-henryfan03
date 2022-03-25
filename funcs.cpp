369e25397b4daf9252a2ad828c246c5c2e73ad70
Henry Fan <henry.fan2313@gmail.com>
2022-03-24 23:01:24 -0400

    added task pixelate

--- funcs.cpp
+++ funcs.cpp
@@ -109,3 +109,54 @@ int frame(std::string filename)
   writeImage("frameImage.pgm",out, h, w);
   return 0;
 }
+
+/*int scale(std::string filename)
+{
+  std::string input = filename;
+  int img[MAX_H][MAX_W];
+  int h, w;
+  readImage(input, img, h, w); // read it from the file "inImage.pgm"
+  // h and w were passed by reference and
+  // now contain the dimensions of the picture
+  // and the 2-dimesional array img contains the image data
+  // Now we can manipulate the image the way we like
+  // for example we copy its contents into a new array
+  int scaledout[h*2][w*2];
+  for(int row = 0; row < h; row += 2) {
+    for(int col = 0; col < w; col++) {
+      scaledout[row][col] = img[row][col];
+      scaledout[row+1][col] = img[row][col];
+    }
+  }
+  // and save this new image to file "outImage.pgm"
+  writeImage("scaleImage.pgm",scaledout, h, w);
+  return 0;
+}*/
+
+int pixelate(std::string filename)
+{
+  std::string input = filename;
+  int img[MAX_H][MAX_W];
+  int h, w;
+  int total, avg;
+  readImage(input, img, h, w); // read it from the file "inImage.pgm"
+  // h and w were passed by reference and
+  // now contain the dimensions of the picture
+  // and the 2-dimesional array img contains the image data
+  // Now we can manipulate the image the way we like
+  // for example we copy its contents into a new array
+  int out[MAX_H][MAX_W];
+  for(int row = 0; row < h; row += 2) {
+    for(int col = 0; col < w; col += 2) {
+      total = img[row][col] + img[row+1][col] + img[row][col+1] + img[row+1][col+1];
+      avg = total / 4;
+      out[row][col] = avg;
+      out[row+1][col] = avg;
+      out[row][col+1] = avg;
+      out[row+1][col+1] = avg;
+    }
+  }
+  // and save this new image to file "outImage.pgm"
+  writeImage("pixelatedimage.pgm",out, h, w);
+  return 0;
+}
--- main.cpp
+++ main.cpp
@@ -10,5 +10,7 @@ int main() {
   invertHalf("image1.pgm");
   box("image1.pgm");
   frame("image1.pgm");
+  //scale("image1.pgm");
+  pixelate("image1.pgm");
   return 0;
 }
